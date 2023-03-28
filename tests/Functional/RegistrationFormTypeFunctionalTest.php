8bd76b5b3ea37b1d4e0579e0b952e3a0ba6d7cb2
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2023-03-28 13:59:19 +0200

    added functionality for functional tests registry form

--- tests/Functional/RegistrationFormTypeFunctionalTest.php
+++ tests/Functional/RegistrationFormTypeFunctionalTest.php
@@ -1,12 +1,46 @@
 <?php
 
+namespace App\Tests\Functional;
+
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
 use Symfony\Component\Form\Test\TypeTestCase;
 use Symfony\Component\Validator\Validation;
 
 
-class RegistrationFormTypeFunctionalTest extends TypeTestCase
+class RegistrationFormTypeFunctionalTest extends WebTestCase
 {
+    public function testSubmitValidData()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
 
+        $form = $crawler->selectButton('Register')->form([
+            'registration_form[email]' => 'test@example.com',
+            'registration_form[agreeTerms]' => true,
+            'registration_form[plainPassword]' => 'password123',
+        ]);
+
+        $client->submit($form);
+
+        $this->assertTrue($client->getResponse()->isRedirect('/'));
+    }
+
+    public function testSubmitInvalidData()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
+
+        $form = $crawler->selectButton('Register')->form([
+            'registration_form[email]' => 'invalidemail',
+            'registration_form[agreeTerms]' => false,
+            'registration_form[plainPassword]' => 'short',
+        ]);
+
+        $client->submit($form);
+
+        $this->assertContains('This value is not a valid email address.', $client->getResponse()->getContent());
+        $this->assertContains('You should agree to our terms.', $client->getResponse()->getContent());
+        $this->assertContains('Your password should be at least 6 characters', $client->getResponse()->getContent());
+    }
 }
